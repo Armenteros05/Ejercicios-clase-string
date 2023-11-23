cfdbab1288ea6614d1100bcc38e0f8d808ba6c1a
Armenteros05 <sarmerod@myuax.com>
2023-11-23 12:10:01 +0100

    1

---
+++ ejercicios 1,2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+string nombre;
+
+int main()
+{
+    cout << "Ingrese su nombre: ";
+    cin >> nombre;
+    cout << "Hola " << nombre << endl;
+    return 0;
+}
