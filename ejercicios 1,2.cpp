5fbd73cedbe73c4d36607625da63a212f036e158
Armenteros05 <sarmerod@myuax.com>
2023-11-23 12:15:45 +0100

    2

--- ejercicios 1,2.cpp
+++ ejercicios 1,2.cpp
@@ -8,9 +8,20 @@ int main()
     cout << "Ingrese su nombre: ";
     cin >> nombre;
     cout << "Hola " << nombre << endl;
-    string frase; "Hola " + nombre;
+    string frase;
+    cout << "Ingrese una frase: ";
+    cin >> frase;
+    cout << "La frase es: " << frase << endl;
     size(frase);
     cout << "La frase tiene " << size(frase) << " caracteres" << endl;
-
+    empty(frase);
+    if (frase.empty())
+    {
+        cout << "La frase esta vacia" << endl;
+    }
+    else
+    {
+        cout << "La frase no esta vacia" << endl;
+    }
     return 0;
 }
